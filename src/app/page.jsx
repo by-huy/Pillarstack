6c28b13c714ba79c8aba29bd38c43108301e9c67
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-12-01 13:48:49 +0700

    text adjustment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -34,7 +34,7 @@ export default async function Home({ searchParams }) {
             for frontend developers and web designers.
           </span>
         </h1>
-        <p className="text-text max-w-lg mx-auto text-base xl:text-h5 2xl:text-h4 2xl:max-w-2xl">
+        <p className="text-text max-w-lg mx-auto text-base xl:text-h4 2xl:text-h3 2xl:max-w-2xl">
           Explore curated and handpicked goodies that enhance your workflow and
           cultivate your growth as a developer and designer.
         </p>
