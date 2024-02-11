8191755ebec8f3d882843e4121bf40a761578964
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2024-02-12 00:21:13 +1100

    pagination styling

--- src/components/Pagination/PaginationControls.jsx
+++ src/components/Pagination/PaginationControls.jsx
@@ -16,25 +16,25 @@ export default function PaginationControls({
     <div>
       <div className="flex justify-center items-center space-x-5 text-base xl:text-h7 2xl:text-h5 mt-8 md:mt-16">
         <button
-            disabled={!hasPrevPage}
-          className="text-accent text-lg font-semibold disabled:text-dim-gray transition-all duration-300 ease-in-out hover:text-gray"
+          disabled={!hasPrevPage}
+          className="text-accent text-lg flex items-center gap-x-1 font-semibold disabled:text-dim-gray transition-all duration-300 ease-in-out hover:text-gray"
           onClick={() => {
             router.push(`?page=${Number(page) - 1}&per_page=${per_page}`);
           }}
         >
-          Previous
+          <span className=" text-base ">&lt;</span> Previous
         </button>
         <div>
           {page} of {Math.ceil(total / Number(per_page))}
         </div>
         <button
-            disabled={!hasNextPage}
-          className="text-accent text-lg font-semibold disabled:text-dim-gray transition-all duration-300 ease-in-out hover:text-gray"
+          disabled={!hasNextPage}
+          className="text-accent text-lg flex items-center gap-x-1 font-semibold disabled:text-dim-gray transition-all duration-300 ease-in-out hover:text-gray"
           onClick={() => {
             router.push(`?page=${Number(page) + 1}&per_page=${per_page}`);
           }}
         >
-          Next
+          Next <span className=" text-base">&gt;</span>
         </button>
       </div>
     </div>
