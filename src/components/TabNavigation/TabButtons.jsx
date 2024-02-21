6638e2a93df341eeb7a2cf0c486fcd704550d5ee
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2024-02-21 21:38:42 +1100

    tab navigation UI & UX sticky

--- src/components/TabNavigation/TabButtons.jsx
+++ src/components/TabNavigation/TabButtons.jsx
@@ -2,7 +2,7 @@
 
 // Note: This component is used to filter resources by category
 import { useRouter, useSearchParams } from "next/navigation";
-
+import { motion } from "framer-motion";
 
 // ... (other imports)
 
@@ -20,38 +20,54 @@ export default function TabButtons({ categories }) {
   const activeCategory = searchParams.get("category") || "";
 
   return (
-    <div className="mb-8 justify-center gap-x-2 flex-wrap gap-y-2 hidden sm:flex">
+    <div className="mb-8 sticky py-5 backdrop-blur-xl bg-opacity-20 top-0 bg-bg z-50 justify-center gap-x-2 flex-wrap gap-y-2 hidden sm:flex">
       {/* Filtering button for "All" category */}
       <button
         aria-label="All categories"
         onClick={() => {
           router.push("/", { scroll: false });
         }}
-        className={`py-1 px-4 flex gap-x-1 font-medium items-center border border-dim-gray rounded-full hover:border-text transition-all text-sm xl:text-h6 ${
-          activeCategory === "" ? "bg-accent text-bg" : " bg-bg text-accent"
+        className={`py-2 px-4 flex gap-x-1 font-medium items-center border border-dim-gray  rounded-xl hover:border-text transition-all text-sm xl:text-h6 ${
+          activeCategory === "" ? "bg-accent text-bg" : "bg-bg text-accent"
         }`}
       >
         All
       </button>
-      {Object.entries(categoryCount).map(([item, count]) => {
-        return (
-          <button
-            key={item}
-            onClick={(e) => {
-              e.preventDefault();
-              router.push(`/?category=${item}`, { scroll: false });
-            }}
-            className={`py-1 px-4 flex gap-x-1 font-medium border border-dim-gray rounded-full hover:border-text transition-all relative ${
-              activeCategory === item ? "text-bg bg-accent" : " bg-bg text-accent"
-            }`}
-          >
-            <span className="text-sm xl:text-h6 ">{item}</span>
-            <span className={`flex justify-center items-center ${activeCategory === item ? "text-bg" : "text-text"} text-xxs xl:text-xs 2xl:text-sm leading-none`}>
-              {count}
-          </span>
-          </button>
-        );
-      })}
+      <div className="flex gap-x-0 bg-dark-charcoal rounded-xl border border-outline border-opacity-15">
+        {Object.entries(categoryCount).map(([item, count]) => {
+          return (
+            <button
+              key={item}
+              onClick={(e) => {
+                e.preventDefault();
+                router.push(`/?category=${item}`, { scroll: false });
+              }}
+              className={`py-2 px-5 flex gap-x-1 font-medium  rounded-xl  transition-all relative ${
+                activeCategory === item
+                  ? ""
+                  : "   hover:text-gray"
+              }`}
+            >
+              {activeCategory === item && (
+                <motion.div
+                  layoutId="active"
+                  className=" absolute inset-0 bg-accent rounded-xl"
+                />
+              )}
+              <span
+                className="text-sm xl:text-h6 relative mix-blend-exclusion  z-10"
+              >
+                {item}
+              </span>
+              <span
+                className="flex relative justify-center mix-blend-exclusion z-10 items-center text-light-gray text-xxs xl:text-xs 2xl:text-sm leading-none"
+              >
+                {count}
+              </span>
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
