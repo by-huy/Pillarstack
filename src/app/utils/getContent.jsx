0aa28eb3adc009fdb3d76087fe5d56bdd05a483d
Alexander-2049 <a.alexander.2049@gmail.com>
2024-03-01 17:09:37 +0200

    Refactor: fixed property names

--- src/app/utils/getContent.jsx
+++ src/app/utils/getContent.jsx
@@ -2,16 +2,16 @@ import { createClient } from "contentful";
 
 //generateStaticParams here to statically generate the contents
 
-export async function getContent({contentType, ...rest}) {
+export async function getContent({ content_type, ...rest }) {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
   });
 
   const {items, total} = await client.getEntries({
-    content_type: contentType,
+    content_type,
     ...rest,
   });
 
-  return {items, total};
+  return { items, total };
 }
